117e727cad0de5c446d8db67ddc876695e91e68e
gabriellesuemi <gabrielle.alencar@unifesp.br>
2026-05-15 14:22:46 -0300

    Criação da estrutura base dos usuários do chat

--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,16 @@
-public class Usuario {
-    
+public abstract class Usuario {
+    protected MediadorChat mediador;
+    protected String nome;
+
+    public Usuario(MediadorChat mediador, String nome) {
+        this.mediador = mediador;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public abstract void enviar(String mensagem);
+    public abstract void receber(String mensagem);
 }
