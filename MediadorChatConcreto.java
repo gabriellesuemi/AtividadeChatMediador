8662c6cf4e183bd05b8114cc35a86f97e2c2f421
gabriellesuemi <gabrielle.alencar@unifesp.br>
2026-05-15 14:54:44 -0300

    Implementação do usuário concreto do chat

--- MediadorChatConcreto.java
+++ MediadorChatConcreto.java
@@ -1,3 +1,21 @@
-public class MediadorChatConcreto {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class MediadorChatConcreto implements MediadorChat{
+
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    @Override
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    @Override
+    public void enviarMensagem(String mensagem, Usuario usuarioRemetente) {
+        for (Usuario usuario : usuarios) {
+            if (usuario != usuarioRemetente) {
+                    usuario.receber(usuarioRemetente.getNome() + ": " + mensagem);
+            }
+        }
+    }
 }
